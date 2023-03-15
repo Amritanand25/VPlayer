503d7870d6f5677b531677bb10c2edb44080463a
amritanand.jnv25@gmail.com <amritanand.jnv25@gmail.com>
2023-03-15 21:48:22 +0530

    added iframe

--- src/Components/WatchPage.js
+++ src/Components/WatchPage.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux';
-import { closeMenu } from '../Utils/appSlice';
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSearchParams } from "react-router-dom";
+import { closeMenu } from "../Utils/appSlice";
 
 const WatchPage = () => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
 
-    useEffect(() => {
-      dispatch(closeMenu());
-    }, []);
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
 
   return (
-    <div>WatchPage</div>
-  )
-}
+    <div className="px-6 py-4">
+      {searchParams && (
+        <iframe
+          width="1000"
+          height="500"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      )}
+    </div>
+  );
+};
 
-export default WatchPage
+export default WatchPage;
